6fb0f103a3985a9bb1b430ee341f6316ca88bc4e
imvipindev <vipinchaudhary1809@gmail.com>
2023-10-19 21:24:44 +0530

    fix: prisma bug fix

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,30 +1,22 @@
-import prismadb from '@/libs/prismadb';
-import { NextResponse } from 'next/server';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '../auth/[...nextauth]/route';
-import { DefaultArgs } from '@prisma/client/runtime/library';
-import { Prisma } from '@prisma/client';
-import { useSearchParams } from 'next/navigation';
+import prismadb from "@/libs/prismadb";
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { DefaultArgs } from "@prisma/client/runtime/library";
+import { Prisma } from "@prisma/client";
+import { useSearchParams } from "next/navigation";
 
 export async function POST(request: Request) {
   try {
     const session = await getServerSession(authOptions);
-    if (!session)
-      return NextResponse.json(
-        { message: 'You are not logged in.' },
-        { status: 401 }
-      );
+    if (!session) return NextResponse.json({ message: "You are not logged in." }, { status: 401 });
 
     const currentUser = await prismadb.user.findUnique({
       where: {
-        email: session.user?.email || '',
+        email: session.user?.email || "",
       },
     });
-    if (!currentUser)
-      return NextResponse.json(
-        { message: 'You are not logged in.' },
-        { status: 401 }
-      );
+    if (!currentUser) return NextResponse.json({ message: "You are not logged in." }, { status: 401 });
 
     const { body } = await request.json();
     const post = await prismadb.post.create({
@@ -37,12 +29,13 @@ export async function POST(request: Request) {
     return NextResponse.json(post, { status: 200 });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ message: 'An Error occurred' }, { status: 500 });
+    return NextResponse.json({ message: "An Error occurred" }, { status: 500 });
   }
 }
+
 export async function GET(request: Request) {
   try {
-    const userId = new URL(request.url).searchParams.get('userId');
+    const userId = new URL(request.url).searchParams.get("userId");
 
     let _args = {
       include: {
@@ -50,24 +43,23 @@ export async function GET(request: Request) {
         comments: true,
       },
       orderBy: {
-        createdAt: 'desc',
+        createdAt: "desc",
       },
-    } as Prisma.PostFindManyArgs; // Changed the type here
+    } as Prisma.UserArgs;
 
-    if (userId && typeof userId === 'string') {
+    if (userId && typeof userId === "string") {
       _args = {
         ..._args,
         where: {
-          userId: userId, // Convert the string to an integer
+          userId,
         },
-      } as Prisma.PostFindManyArgs; // Changed the type here
+      } as Prisma.UserArgs;
     }
 
     const posts = await prismadb.post.findMany(_args);
     return NextResponse.json(posts, { status: 200 });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ message: 'An Error occurred' }, { status: 500 });
-    
+    return NextResponse.json({ message: "An Error occurred" }, { status: 500 });
   }
 }
